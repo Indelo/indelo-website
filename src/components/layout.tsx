047eecbd7d97accbbd482d358b9ae1f91242099e
Nicol Vojacek <nvojacek@gmail.com>
2020-07-05 14:00:26 +0200

    Update src/components/layout.tsx
    Co-authored-by: Jason Russell <jasrusable@users.noreply.github.com>

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import { Container } from 'semantic-ui-react';
 import { Navigation } from './navigation';
 import { Footer } from './footer';
 
-export const Layout = ({ children }: { children: any }) => {
+export const Layout = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
   return (
     <div>
       <Navigation />
