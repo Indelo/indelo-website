d7510647ae137e17bb6bbabd8b7eda276eaad651
Jason Russell <jasrusable@gmail.com>
2020-07-07 22:37:12 +0200

    Fix linting

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import { Button, Grid, Segment, Form } from 'semantic-ui-react';
 import { Section } from './section';
