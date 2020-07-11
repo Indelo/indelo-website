455a69ad2582f5f50a211dae93f8cdf35c39aab6
Jason Russell <jasrusable@gmail.com>
2020-07-11 19:03:57 +0200

    Add logos

--- src/components/technology-stack.tsx
+++ src/components/technology-stack.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
-import _ from 'lodash';
-import { Grid, Image } from 'semantic-ui-react';
+import _, { chunk } from 'lodash';
+import { Grid, Image, Header, Segment } from 'semantic-ui-react';
 import { Section } from './section/section';
+import Node from '../assets/svgs/nodejs-icon.svg';
+import JavaScript from '../assets/svgs/javascript.svg';
+import TypeScript from '../assets/svgs/typescript-icon.svg';
+import Express from '../assets/svgs/express.svg';
+import Python from '../assets/svgs/python.svg';
 
-const columns = _.times(5, (i) => (
-  <Grid.Column key={i}>
-    <Image
-      src="https://react.semantic-ui.com/images/wireframe/square-image.png"
-      size="medium"
-    />
-  </Grid.Column>
-));
+const items = [
+  {
+    title: 'Node',
+    image: <Node />,
+  },
+  {
+    title: 'JavaScript',
+    image: <JavaScript />,
+  },
+  {
+    title: 'TypeScript',
+    image: <TypeScript />,
+  },
+  {
+    title: 'Express',
+    image: <Express />,
+  },
+  {
+    title: 'Python',
+    image: <Python />,
+  },
+];
 
 export const TechnologyStack = () => {
   return (
     <Section title="Our technology stack">
-      <Grid columns={5}>
-        <Grid.Row>{columns}</Grid.Row>
-        <Grid.Row>{columns}</Grid.Row>
-      </Grid>
+      <Segment>
+        <Grid columns={8}>
+          {chunk(items, 8).map((chunks) => (
+            <Grid.Row>
+              {chunks.map(({ title, image }) => {
+                return (
+                  <Grid.Column>
+                    <Header textAlign="center">{title}</Header>
+                    <Image>{image}</Image>
+                  </Grid.Column>
+                );
+              })}
+            </Grid.Row>
+          ))}
+        </Grid>
+      </Segment>
     </Section>
   );
 };
