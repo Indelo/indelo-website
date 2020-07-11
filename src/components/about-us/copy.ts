a3113d05a8764a8fd0b4624a65a05bea7ece1584
Nicol Vojacek <nvojacek@gmail.com>
2020-07-11 18:41:59 +0200

    Update about the team section to be responsive

---
+++ src/components/about-us/copy.ts
@@ -0,0 +1,34 @@
+import Jason from '../assets/images/jason.jpeg';
+import Megan from '../assets/images/megan.jpeg';
+import Nicol from '../assets/images/nicol.jpeg';
+import George from '../assets/images/george.jpeg';
+
+export const team = [
+  {
+    name: 'Megan',
+    image: Megan,
+    body:
+      'Front-end developer and designer. Holds a degree in Visual Communication and Design (Hons) from Stellenbosch University.',
+  },
+  {
+    name: 'Jason',
+    image: Jason,
+    body:
+      'Full-stack developer with a degree in Engineering from UCT and over 10 years of experience developing software and running businesses',
+  },
+  {
+    name: 'Nicol',
+    image: Nicol,
+    body:
+      'Full-stack developer with a degree in Computer Science and Business Computing from UCT. A jack of all trades, with a particular love for React',
+  },
+  {
+    name: 'George',
+    image: George,
+    body:
+      'Keeps everyone happy and motivated. George is notorious for lap-cuddles and sleeping on keyboards',
+  },
+];
+
+export const aboutUsBody =
+  'We are a team of passionate and driven developers, designers and UI/UX professionals with a variety of industry experience. We love building things and creating beautiful products that work well. We focus on the details to ensure our products are user friendly, unique and exactly what you ask for. We thrive to empower businesses through technology!';
