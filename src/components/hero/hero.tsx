4d9e0538561ef149e78e5012dd66a9b80b5e0c17
Nicol Vojacek <nvojacek@gmail.com>
2020-07-12 15:59:23 +0200

    Sample 1

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -5,7 +5,7 @@ import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
 import css from './hero.module.css';
 import logo from '../../assets/images/test.gif';
-import test from '../../assets/test3.svg';
+import test from '../../assets/test20.svg';
 import smallLogo from '../../assets/images/small-logo.png';
 
 const query = graphql`
