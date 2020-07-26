275244760fa01d33b04a21a4ff66d6230b565b55
Jason Russell <jasrusable@gmail.com>
2020-07-26 16:35:34 +0200

    Remove contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,27 +3,6 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
-const contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-};
-
-// if you want to use the preview API please define
-// CONTENTFUL_HOST in your environment config
-// the `host` property should map to `preview.contentful.com`
-// https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
-if (process.env.CONTENTFUL_HOST) {
-  contentfulConfig.host = process.env.CONTENTFUL_HOST;
-}
-
-const { spaceId, accessToken } = contentfulConfig;
-
-if (!spaceId || !accessToken) {
-  throw new Error(
-    'Contentful spaceId and the access token need to be provided.',
-  );
-}
-
 module.exports = {
   siteMetadata: {
     title: 'Indelo | Development agency',
@@ -52,11 +31,6 @@ module.exports = {
       options: {
         precachePages: ['/*'],
       },
-    },
-    // CMS
-    {
-      resolve: 'gatsby-source-contentful',
-      options: contentfulConfig,
     },
     {
       resolve: 'gatsby-source-filesystem',
