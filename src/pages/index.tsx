eb2bd8894b5de9bdb867618c183ba6d75d8c11f5
Jason Russell <jasrusable@gmail.com>
2020-07-28 22:08:05 +0200

    Testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,6 +38,7 @@ const RootIndex = () => {
             <Container>
               <Segment basic padded>
                 <Hero />
+                Test
               </Segment>
             </Container>
           </div>
